96fe21bbd0090cd2d1a4067bc2f7e14107836bd5
Oliwier Markiewicz <101707516+ol1mowski@users.noreply.github.com>
2023-05-20 14:17:43 +0200

    add animation to subpage

--- scripts/app-second-pade.js
+++ scripts/app-second-pade.js
@@ -15,3 +15,26 @@ elements[3].addEventListener('click', (e) => {
     e.preventDefault();
     window.location = '../index.html#contact';
 });
+
+
+const box1 = document.querySelector('.main-section__content__header__h2');
+const text1 = "Webpage Computer store";
+let worldIndex1 = 0;
+
+let oldTime = 0;
+const speed = 100;
+
+const typing = (newTime) => {
+    if (newTime - oldTime > speed) {
+        oldTime = newTime;
+        if (worldIndex1 < text1.length) {
+            const letter = text1.substr(worldIndex1, 1);
+            box1.textContent += letter;
+            worldIndex1++;
+        }
+    }
+
+    requestAnimationFrame(typing);
+}
+
+typing();
