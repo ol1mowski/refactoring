b4abd6c94bd86feb8ceb4b52809dc1b99d0a59e0
Oliwier Markiewicz <101707516+ol1mowski@users.noreply.github.com>
2023-05-26 18:37:40 +0200

    add correctyl href to link

---
+++ scripts/write animation/tic-tac.js
@@ -0,0 +1,21 @@
+const box1 = document.querySelector('.main-section__content__header__h2');
+const text1 = "Tic-tac-toe game";
+let worldIndex1 = 0;
+
+let oldTime = 0;
+const speed = 100;
+
+const typing = (newTime) => {
+    if (newTime - oldTime > speed) {
+        oldTime = newTime;
+        if (worldIndex1 < text1.length) {
+            const letter = text1.substr(worldIndex1, 1);
+            box1.textContent += letter;
+            worldIndex1++;
+        }
+    }
+
+    requestAnimationFrame(typing);
+}
+
+typing();
